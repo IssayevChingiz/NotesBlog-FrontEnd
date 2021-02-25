bfa14ee8cd7c4ba577f6af7918648a4886118e02
issayev <issayev@sheridancollege.ca>
2021-02-25 13:38:20 -0500

    api component refactored

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,14 +5,11 @@ import * as api from "../api/index.js";
 export const signin = (formData, history) => async (dispatch) => {
   try {
     const { data } = await api.signIn(formData);
-    //console.log(data);
 
-    //dispatch({ type: AUTH, data: data.data })
     dispatch({ type: AUTH, data });
 
     history.push("/");
   } catch (e) {
-    //console.log(e.response.data.message);
     dispatch({ type: ERROR, data: e.response.data.message });
   }
 };
